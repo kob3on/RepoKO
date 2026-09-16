7205ce2ac296a338f109b4e7feedf739dcb3f209
kob3on <kobzevvaceslav2644@gmail.com>
2026-09-16 19:02:43 +0500

    hello world colorful

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Привет, мир!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("hi lol");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("hi lol");
+            Console.ReadKey();
+        }
+    }
+}
