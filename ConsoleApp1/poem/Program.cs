d6c9cf0193d378ebabb3d10fe4a0ac3ecdf43dbf
kob3on <kobzevvaceslav2644@gmail.com>
2026-09-23 11:40:06 +0500

    стих

---
+++ ConsoleApp1/poem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Белеет парус одинокой");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("В тумане моря голубом!..");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Что ищет он в стране далекой?");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Что кинул он в краю родном?...");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Играют волны — ветер свищет,");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("И мачта гнется и скрыпит...");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Увы! Он счастия не ищет");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("И не от счастия бежит!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Под ним струя светлей лазури,");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Над ним луч солнца золотой...");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("А он, мятежный, просит бури,");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Как будто в бурях есть покой!");
+        }
+    }
+}
